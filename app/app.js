3bbaa174258005bb8c8904e080dbff0b13f3ea4a
Viren Bhagat <virenb@gmail.com>
2016-06-09 18:56:52 -0400

    Removed non-interactive map, added JavaScript and Leaflet.js

---
+++ app/app.js
@@ -0,0 +1,28 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'mapbox.streets'
+}).addTo(mymap);
+
+
+
+var popup = L.popup();
+
+      function onMapMove(e) {
+        var lat = mymap.getCenter().lat.toFixed(3);
+        var lng = mymap.getCenter().lng.toFixed(3);
+        console.log("You are at " + lat + ", " + lng);
+        $.getJSON("http://api.sunrise-sunset.org/json", {
+          lat: lat,
+          lng: lng
+        }, function (results) {
+          $("#sunrise").text(results.results.sunrise);
+          $("#sunset").text(results.results.sunset);
+        });
+      }
+
+mymap.on('move', onMapMove);

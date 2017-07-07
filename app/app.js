6fe1de2cb2525c9702467c5bbaf2e95a4f3326d9
Viren Bhagat <virenb@gmail.com>
2017-07-07 14:12:51 -0400

    Adding leaflet package

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 var mymap = L.map('mapid').setView([37.7749, -122.4194], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
 	maxZoom: 18,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	id: 'mapbox.streets',
-  accessToken: 'pk.eyJ1IjoidmlyZW5iIiwiYSI6ImNqNHU1MXljcDBmczMyd21jYTJuZjZqN20ifQ.GUKFDDDMgcBn5IhwSQyDNQ'
+	id: 'mapbox.streets'
 }).addTo(mymap);
 
 

883d8f9fed4a1938e38c46272a2e02c9fe6852b4
Viren Bhagat <virenb@gmail.com>
2017-07-07 13:22:32 -0400

    Updated js files with new leaflet cdn

---
+++ build/sun-api.1.0.0.js
@@ -0,0 +1,77 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	var mymap = L.map('mapid').setView([37.7749, -122.4194], 13);
+	
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+	  maxZoom: 18,
+	  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	  id: 'mapbox.streets'
+	}).addTo(mymap);
+	
+	var popup = L.popup();
+	
+	function onMapMove(e) {
+	  var lat = mymap.getCenter().lat.toFixed(3);
+	  var lng = mymap.getCenter().lng.toFixed(3);
+	  // console.log("You are at " + lat + ", " + lng);
+	  $("#latlong").text(lat + ", " + lng);
+	  $.getJSON("http://api.sunrise-sunset.org/json", {
+	    lat: lat,
+	    lng: lng
+	  }, function (results) {
+	    $("#sunrise").text(results.results.sunrise);
+	    $("#sunset").text(results.results.sunset);
+	  });
+	}
+	
+	mymap.on('move', onMapMove);
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=sun-api.1.0.0.js.map

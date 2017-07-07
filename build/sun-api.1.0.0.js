82a9992b1f6a14e66eb66e12f320a2c676b5e896
Viren Bhagat <virenb@gmail.com>
2017-07-07 14:05:16 -0400

    ran build with updated access token, etc

--- build/sun-api.1.0.0.js
+++ build/sun-api.1.0.0.js
@@ -48,10 +48,11 @@
 	
 	var mymap = L.map('mapid').setView([37.7749, -122.4194], 13);
 	
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	  maxZoom: 18,
 	  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	  id: 'mapbox.streets'
+	  id: 'mapbox.streets',
+	  accessToken: 'pk.eyJ1IjoidmlyZW5iIiwiYSI6ImNqNHU1MXljcDBmczMyd21jYTJuZjZqN20ifQ.GUKFDDDMgcBn5IhwSQyDNQ'
 	}).addTo(mymap);
 	
 	var popup = L.popup();
@@ -61,7 +62,7 @@
 	  var lng = mymap.getCenter().lng.toFixed(3);
 	  // console.log("You are at " + lat + ", " + lng);
 	  $("#latlong").text(lat + ", " + lng);
-	  $.getJSON("http://api.sunrise-sunset.org/json", {
+	  $.getJSON("https://api.sunrise-sunset.org/json", {
 	    lat: lat,
 	    lng: lng
 	  }, function (results) {

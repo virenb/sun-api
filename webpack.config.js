38e60e9a9cc89e673d1b948dcf3c4bc5a56eeef4
Viren Bhagat <virenb@gmail.com>
2017-07-13 10:16:28 -0400

    Changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,11 @@
 var path = require('path');
 
-var webpack = require('webpack');
-
-var packageData = require('./package.json');
-
-var filename = [packageData.name, packageData.version, 'js'];
-
 module.exports = {
     entry: './app/app.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: filename.join('.'),
-    },
-    devtool: 'source-map',
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel',
-          query: {
-            presets: ['es2015']
-          }
-        }
-      ]
+        filename: 'bundle.js',
+        path: path.resolve('build')
+
     }
+    
 }
